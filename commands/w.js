e3e7b244894da98da4712d25ef8ebaca24a7c0fe
Ahmed <ahmedo0901658@gmail.com>
2025-11-22 11:20:21 +0200

    Refactor 'w' command to fetch random anime girl image

--- commands/w.js
+++ commands/w.js
@@ -1,31 +1,29 @@
-const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const fetch = require('node-fetch');
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("w")
-    .setDescription("يرسل صورة بنت أنمي عشوائية"),
+    data: new SlashCommandBuilder()
+        .setName('w')
+        .setDescription('صورة بنت أنمي عشوائية'),
 
-  async execute(interaction) {
-    const images = [
-      "https://i.imgur.com/x1.jpg",
-      "https://i.imgur.com/x2.jpg",
-      "https://i.imgur.com/x3.jpg"
-    ];
+    async execute(interaction) {
+        await interaction.deferReply();
 
-    const random = images[Math.floor(Math.random() * images.length)];
+        const res = await fetch('https://api.waifu.pics/sfw/waifu');
+        const data = await res.json();
 
-    const embed = new EmbedBuilder()
-      .setTitle("💗 صورة بنت أنمي")
-      .setImage(random)
-      .setColor("#ff7bd3");
+        const embed = new EmbedBuilder()
+            .setTitle('💗 صورة بنت أنمي')
+            .setImage(data.url)
+            .setColor('#ff66b2');
 
-    const row = new ActionRowBuilder().addComponents(
-      new ButtonBuilder()
-        .setCustomId("vote_w")
-        .setLabel("👍 إعجاب")
-        .setStyle(ButtonStyle.Primary)
-    );
+        const btn = new ActionRowBuilder().addComponents(
+            new ButtonBuilder()
+                .setCustomId('like_w')
+                .setLabel('إعجاب 👍')
+                .setStyle(ButtonStyle.Primary)
+        );
 
-    await interaction.reply({ embeds: [embed], components: [row] });
-  },
+        await interaction.editReply({ embeds: [embed], components: [btn] });
+    }
 };
